2228337476ec5768df334691a1f49f80cb80307e
yuyifanruanjian <291353485@qq.com>
2018-06-10 18:29:06 +0800

    v8.15

--- balance-transfer/model/photo.js
+++ balance-transfer/model/photo.js
@@ -55,30 +55,42 @@ var submitPhoto = async function (res, photo) {
                 }
             },
             async function (callback) {
-                if (response.success == false) {
+                if (response.message == '照片已经存在') {
                     callback(null);
                 } else {
                     var sql = {};
                     sql.sql = 'update user set score=score+? where id=?';
                     sql.sqlData = [photo.score, photo.id];
                     db.connection.query(sql.sql, sql.sqlData, async function(err, results1) {
-                        var queryq = {
-                            wants:'id',
-                            table:'photo',
-                            conditions:{
-                                userId:[photo.id],
-                                photoUrl:[photo.photoUrl],
-                                carId:[photo.carId],
-                                score:[score],
-                                name:[photo.content],
-                                ttime:[photo.ttime],
-                            }
-                        };
-                        let sql = await db.sqlSelect(queryq);
-                        db.connection.query(sql.sql, sql.sqlData, async function(err, results2) {
-                            response.success = true;
-                            callback(err, results2);
-                        });
+                        callback(err);
+                    });
+                }
+            },
+            async function (callback) {
+                if (response.message == '照片已经存在') {
+                    callback(null);
+                } else {
+                    if (photo.goodOrBad=='坏') {
+                        var score = photo.score * (-1);
+                    } else {
+                        var score = photo.score;
+                    }
+                    var queryq = {
+                        wants:'id',
+                        table:'photo',
+                        conditions:{
+                            userId:[photo.id],
+                            photoUrl:[photo.photoUrl],
+                            carId:[photo.carId],
+                            score:[score],
+                            name:[photo.content],
+                            ttime:[photo.ttime],
+                        }
+                    };
+                    let sql = await db.sqlSelect(queryq);
+                    db.connection.query(sql.sql, sql.sqlData, async function(err, results2) {
+                        response.success = true;
+                        callback(err, results2);
                     });
                 }
             }
